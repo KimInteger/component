f74e94889bd61e4604d469b20dd8da0db1e04d62
KimInteger <kjs20151240@gmail.com>
2024-06-11 11:11:51 +0900

    decision이란 함수를 생성해서 formatHTML을 핸들링

--- work-1.js
+++ work-1.js
@@ -1,1 +1,34 @@
-console.log('work-1');
+function formatHTML(language) {
+  let htmlData = {
+    titleOne:'김정수',
+    titleTwo : "KimInteger",
+    h1:"내 이름은",
+    p: "인티저 입니다."
+  };
+
+  let decision = () => {
+    if (language === "korean") {
+      return htmlData.titleOne;
+    } else if (language === 'english') {
+      return htmlData.titleTwo;
+    }
+  };
+
+  let html = `
+  <!DOCTYPE html>
+  <html lang=ko>
+    <head>
+      <title>${decision()}</title>
+    </head>
+    <body>
+      <h1>${htmlData.h1}</h1>
+      <p>${htmlData.p}</p>
+    </body>
+  </html>
+  `
+
+  return html;
+};
+
+console.log(formatHTML('korean'));
+console.log(formatHTML('english'));
