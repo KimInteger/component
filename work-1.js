bc25dbac4f91c890e9cade702ac429b3579cc255
KimInteger <kjs20151240@gmail.com>
2024-06-11 11:41:04 +0900

    이해를 돕기위한 주석 생성

--- work-1.js
+++ work-1.js
@@ -1,4 +1,8 @@
-function formatHTML(language) {
+// ! default params
+// 매개변수 안쓸 때도 있을 떄를 대비하여 디폴트 파라미터라는 것이 있다.
+// 매개변수 = 대체될 값을 적으면된다.
+
+function formatHTML(language = 'korean') {
   let htmlData = {
     titleOne:'김정수',
     titleTwo : "KimInteger",
@@ -6,6 +10,7 @@ function formatHTML(language) {
     p: "인티저 입니다."
   };
 
+  // * helper function
   let decision = () => {
     if (language === "korean") {
       return htmlData.titleOne;
